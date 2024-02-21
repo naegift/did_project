3e11627c59cfed9c74e86db68eef4cb385efbcc3
subin <github@subin.kr>
2024-02-21 17:04:06 +0900

    Init: Default API setting

--- backend/src/market/store/store.controller.ts
+++ backend/src/market/store/store.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { StoreService } from './store.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Market | Store')
 @Controller('store')
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
+
+  @Get()
+  @ApiOperation({ summary: '[작업중] Get Products' })
+  async getProducts(
+    @Query('seller') seller: string,
+    @Query('page', ParseIntPipe) page: number,
+  ) {}
 }
