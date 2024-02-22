1be1328d0469dcf6cabb6bd1985d8dc8504fc5b6
subin <github@subin.kr>
2024-02-22 18:35:05 +0900

    Init: getProducts

--- backend/src/market/store/store.controller.spec.ts
+++ backend/src/market/store/store.controller.spec.ts
@@ -1,20 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
+import { providers } from 'src/__base-code__/mock/providers/providers';
+import { ProductModel } from 'src/__base-code__/entity/product.entity';
+import { MockProductModel } from 'src/__base-code__/mock/entity/product.mock';
 
 describe('StoreController', () => {
   let controller: StoreController;
+  let service: StoreService;
+  let product: ProductModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StoreController],
-      providers: [StoreService],
+      providers: providers,
     }).compile();
 
     controller = module.get<StoreController>(StoreController);
+    service = module.get<StoreService>(StoreService);
+    product = new MockProductModel().product;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('Get Products', () => {
+    it('Use | getProducts', async () => {
+      service.getProducts = jest.fn();
+      await controller.getProducts(product.seller, 1);
+      expect(service.getProducts).toHaveBeenCalled();
+    });
   });
 });
