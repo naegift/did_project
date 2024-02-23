c9b772dbb7eef98a1cf6d92ec7a700aafa27b931
boram-blockchain <penguniee@gmail.com>
2024-02-23 17:02:58 +0900

    feat : modal component, main page data, view page data

--- frontend/src/images/Banner/index.tsx
+++ frontend/src/images/Banner/index.tsx
@@ -1,3 +1,4 @@
 import bannerImg1 from "./banner1.png";
+import bannerImg2 from "./banner2.png";
 
-export { bannerImg1 };
+export { bannerImg1, bannerImg2 };
--- frontend/src/pages/View.tsx
+++ frontend/src/pages/View.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 import ViewBox from "../components/molecules/ViewBox";
 
+export interface Product {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  price: number;
+  seller: string;
+}
+
 const View: React.FC = () => {
+  const [product, setProduct] = useState<Product[]>([]);
+  const { id } = useParams();
+
+  useEffect((): void => {
+    const productData = async () => {
+      try {
+        const response = await axios.get<Product>(
+          `https://naegift.subin.kr/product/${id}`
+        );
+        console.log(response.data);
+        setProduct([response.data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    productData();
+  }, []);
+
   return (
     <>
-      <ViewBox />
+      <ViewBox product={product} />
     </>
   );
 };
