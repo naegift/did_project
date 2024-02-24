3d70b9a2a13e41d289f9175994aada195e27878f
subin <github@subin.kr>
2024-02-24 16:12:24 +0900

    Fix: Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: ['localhost:3000'] },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('NaeGift')
