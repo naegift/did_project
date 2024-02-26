aa1319f6f067909f2fa66b397449b742acbc12fe
subin <github@subin.kr>
2024-02-26 14:58:24 +0900

    Delete: uploadImage API

--- backend/src/common/image/image.controller.spec.ts
+++ backend/src/common/image/image.controller.spec.ts
@@ -1,40 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ImageController } from './image.controller';
-import { providers } from 'src/__base-code__/mock/providers/providers';
-import { ImageService } from './image.service';
-import { Readable } from 'typeorm/platform/PlatformTools';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { ImageController } from './image.controller';
+// import { providers } from 'src/__base-code__/mock/providers/providers';
+// import { ImageService } from './image.service';
+// import { Readable } from 'typeorm/platform/PlatformTools';
 
-describe('ImageController', () => {
-  let controller: ImageController;
-  let service: ImageService;
-  const emptyFile = {
-    fieldname: '',
-    originalname: '',
-    encoding: '',
-    mimetype: '',
-    size: 1,
-    stream: new Readable(),
-    destination: '',
-    filename: '',
-    path: '',
-    buffer: Buffer.alloc(1),
-  };
+// describe('ImageController', () => {
+//   let controller: ImageController;
+//   let service: ImageService;
+//   const emptyFile = {
+//     fieldname: '',
+//     originalname: '',
+//     encoding: '',
+//     mimetype: '',
+//     size: 1,
+//     stream: new Readable(),
+//     destination: '',
+//     filename: '',
+//     path: '',
+//     buffer: Buffer.alloc(1),
+//   };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ImageController],
-      providers,
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [ImageController],
+//       providers,
+//     }).compile();
 
-    controller = module.get<ImageController>(ImageController);
-    service = module.get<ImageService>(ImageService);
-  });
+//     controller = module.get<ImageController>(ImageController);
+//     service = module.get<ImageService>(ImageService);
+//   });
 
-  describe('Upload Image', () => {
-    it('Use | uploadImage', async () => {
-      service.uploadImage = jest.fn();
-      await controller.uploadImage(emptyFile);
-      expect(service.uploadImage).toHaveBeenCalled();
-    });
-  });
-});
+//   describe('Upload Image', () => {
+//     it('Use | uploadImage', async () => {
+//       service.uploadImage = jest.fn();
+//       await controller.uploadImage(emptyFile);
+//       expect(service.uploadImage).toHaveBeenCalled();
+//     });
+//   });
+// });
--- backend/src/common/image/image.controller.ts
+++ backend/src/common/image/image.controller.ts
@@ -1,44 +1,28 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ImageService } from './image.service';
-import {
-  ApiBody,
-  ApiConsumes,
-  ApiCreatedResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ResUploadImage } from './dto/res-upload-image.dto';
-import { plainToInstance } from 'class-transformer';
 
-@ApiTags('Common | Image')
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
-  @Post()
-  @UseInterceptors(FileInterceptor('file'))
-  @ApiOperation({ summary: '이미지 업로드' })
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
-  @ApiCreatedResponse({ type: ResUploadImage })
-  uploadImage(@UploadedFile() file: Express.Multer.File): ResUploadImage {
-    const result = this.imageService.uploadImage(file);
-    return plainToInstance(ResUploadImage, result);
-  }
+  // @Post()
+  // @UseInterceptors(FileInterceptor('file'))
+  // @ApiOperation({ summary: '이미지 업로드' })
+  // @ApiConsumes('multipart/form-data')
+  // @ApiBody({
+  //   schema: {
+  //     type: 'object',
+  //     properties: {
+  //       file: {
+  //         type: 'string',
+  //         format: 'binary',
+  //       },
+  //     },
+  //   },
+  // })
+  // @ApiCreatedResponse({ type: ResUploadImage })
+  // uploadImage(@UploadedFile() file: Express.Multer.File): ResUploadImage {
+  //   const result = this.imageService.uploadImage(file);
+  //   return plainToInstance(ResUploadImage, result);
+  // }
 }
