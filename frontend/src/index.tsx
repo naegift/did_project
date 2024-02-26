7ef48cb0daa3d3c4238c8c1cbf412c571a850cc3
boram-blockchain <penguniee@gmail.com>
2024-02-26 18:09:05 +0900

    fix: main, view page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <RecoilRoot>
+    <Router>
+      <App />
+    </Router>
+  </RecoilRoot>
 );
