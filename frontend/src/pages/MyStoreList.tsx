5ec61e65eae8b9131cc6dd9770f25f8279497a2b
sangheun969 <sang969@naver.com>
2024-02-29 15:45:33 +0900

    fix REACT_APP_AWS in Main

--- frontend/src/pages/MyStoreList.tsx
+++ frontend/src/pages/MyStoreList.tsx
@@ -1,7 +1,4 @@
-import React, {
-  useEffect,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 import MyProductList from "../components/templates/MyProductList";
 import axios from "axios";
 import { ethers } from "ethers";
@@ -21,49 +18,30 @@ export interface Product {
 }
 
 const MyStoreList: React.FC = () => {
-  const [product, setProduct] =
-    useState<Product[]>([]);
-  const [userAddress, setUserAddress] =
-    useState<string>("");
-  const [page, setPage] =
-    useState<number>(1);
-  const [order, setOrder] =
-    useState<string>("desc");
-  const [seller, setSeller] =
-    useState<string>("");
+  const [product, setProduct] = useState<Product[]>([]);
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const [order, setOrder] = useState<string>("desc");
+  const [seller, setSeller] = useState<string>("");
 
   useEffect(() => {
-    const getWalletAddress =
-      async () => {
-        if (window.ethereum) {
-          try {
-            await window.ethereum.request(
-              {
-                method:
-                  "eth_requestAccounts",
-              }
-            );
-            const provider =
-              new ethers.providers.Web3Provider(
-                window.ethereum
-              );
-            const signer =
-              provider.getSigner();
-            const userAddress =
-              await signer.getAddress();
-            setSeller(userAddress);
-          } catch (error) {
-            console.log(
-              "자갑주소 가져오기 에러",
-              error
-            );
-          }
-        } else {
-          console.log(
-            "메타마스크 설치하십시오"
-          );
+    const getWalletAddress = async () => {
+      if (window.ethereum) {
+        try {
+          await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const userAddress = await signer.getAddress();
+          setSeller(userAddress);
+        } catch (error) {
+          console.log("자갑주소 가져오기 에러", error);
         }
-      };
+      } else {
+        console.log("메타마스크 설치하십시오");
+      }
+    };
 
     getWalletAddress();
   }, []);
@@ -71,22 +49,16 @@ const MyStoreList: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response =
-          await axios.get<Data>(
-            `${
-              process.env
-                .REACT_APP_API ||
-              process.env.REACT_APP_AWS
-            }/store?seller=${seller}&page=${page}&order=${order}`
-          );
-        setProduct(
-          response.data.products
+        const response = await axios.get<Data>(
+          `${
+            // process.env
+            //   .REACT_APP_API ||
+            process.env.REACT_APP_AWS
+          }/store?seller=${seller}&page=${page}&order=${order}`
         );
+        setProduct(response.data.products);
       } catch (error) {
-        console.error(
-          "데이터를 불러오는 중 에러 발생:",
-          error
-        );
+        console.error("데이터를 불러오는 중 에러 발생:", error);
       }
     };
 
@@ -99,10 +71,7 @@ const MyStoreList: React.FC = () => {
     <div>
       <h1>Store</h1>
       <div>Address : {seller}</div>
-      <MyProductList
-        products={product}
-        userAddress={userAddress}
-      />
+      <MyProductList products={product} userAddress={userAddress} />
     </div>
   );
 };
