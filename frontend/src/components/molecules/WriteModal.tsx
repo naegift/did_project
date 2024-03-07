84b54e56a755a4c69bcc88da95e560b29873a855
Eunjae Lee <vanitasium@gmail.com>
2024-03-07 14:16:08 +0900

    fix: axios url 수정

--- frontend/src/components/molecules/WriteModal.tsx
+++ frontend/src/components/molecules/WriteModal.tsx
@@ -16,7 +16,9 @@ interface ModalProps {
   onSubmit: () => Promise<void>;
 }
 
-const WriteModal: React.FC<ModalProps> = ({
+const WriteModal: React.FC<
+  ModalProps
+> = ({
   onClose,
   title,
   content,
@@ -25,54 +27,99 @@ const WriteModal: React.FC<ModalProps> = ({
   price,
   // userWalletAddress,
 }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const handleRegistration = async () => {
-    onClose();
-    setIsLoading(true);
-    const ethPrice = ethers.utils.parseUnits(price, "ether").toString();
-    try {
-      const { message, signature } = await runEthers(title, content, ethPrice);
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("title", title);
-      formData.append("content", content);
-      formData.append("price", ethPrice);
-      formData.append("signature", signature);
+  const [isLoading, setIsLoading] =
+    useState(false);
+  const handleRegistration =
+    async () => {
+      onClose();
+      setIsLoading(true);
+      const ethPrice = ethers.utils
+        .parseUnits(price, "ether")
+        .toString();
+      try {
+        const { message, signature } =
+          await runEthers(
+            title,
+            content,
+            ethPrice
+          );
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("title", title);
+        formData.append(
+          "content",
+          content
+        );
+        formData.append(
+          "price",
+          ethPrice
+        );
+        formData.append(
+          "signature",
+          signature
+        );
 
-      // formData.append("seller", userWalletAddress);
+        // formData.append("seller", userWalletAddress);
 
-      const response = await axios.post(
-        `${process.env.REACT_APP_AWS}/product`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+        const response =
+          await axios.post(
+            `${
+              process.env
+                .REACT_APP_API ||
+              process.env.REACT_APP_AWS
+            }/product`,
+            formData,
+            {
+              headers: {
+                "Content-Type":
+                  "multipart/form-data",
+              },
+            }
+          );
 
-      console.log("Product registered:", response.data);
+        console.log(
+          "Product registered:",
+          response.data
+        );
 
-      const productId = response.data.id;
-      console.log(file, title, content, price, signature);
+        const productId =
+          response.data.id;
+        console.log(
+          file,
+          title,
+          content,
+          price,
+          signature
+        );
 
-      window.location.href = `/product/${productId}`;
-    } catch (error) {
-      console.error("Error registering product:", error);
-    } finally {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 2000);
-    }
-  };
+        window.location.href = `/product/${productId}`;
+      } catch (error) {
+        console.error(
+          "Error registering product:",
+          error
+        );
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      }
+    };
   return (
     <div className="fixed inset-0 bg-black opacity-80">
       {isLoading && <Loading />}
       <div className="w-full y-full flex justify-center flex-col items-center mt-[100px] gap-y-10">
-        <p className="text-white">Title: {title}</p>
-        <p className="text-white text-wrap">Image: {file ? file.name : ""}</p>
-        <p className="text-white">Price: {price} eth</p>
-        <p className="text-white text-wrap">Content: {content}</p>
+        <p className="text-white">
+          Title: {title}
+        </p>
+        <p className="text-white text-wrap">
+          Image: {file ? file.name : ""}
+        </p>
+        <p className="text-white">
+          Price: {price} eth
+        </p>
+        <p className="text-white text-wrap">
+          Content: {content}
+        </p>
       </div>
       <div className="flex flex-row  justify-center my-[50px] gap-x-5">
         <Button
