1d8c5c607f300fcf5301a5666f9cafd49514398d
subin <github@subin.kr>
2024-03-08 10:22:10 +0900

    Fix: Swagger description

--- backend/src/common/vc/vc.controller.ts
+++ backend/src/common/vc/vc.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { VcService } from './vc.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('vc')
+@ApiTags('TEST | VC')
 export class VcController {
   constructor(private readonly vcService: VcService) {}
 
   @Get()
+  @ApiOperation({ summary: 'VC 로직 테스트' })
   async getID() {
     return this.vcService.getID();
   }
 
   @Get('credentialStatus')
+  @ApiOperation({ summary: '테스트 VC의 상태 API' })
   async getCredentialStatus() {
     return false;
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,11 +14,12 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('NaeGift')
     .setDescription('KGA DID project.')
-    .setVersion('0.10.1')
+    .setVersion('0.11.0')
     .addTag('Market')
     .addTag('Market | Product')
     .addTag('Market | Store')
     .addTag('Account | Gift')
+    .addTag('TEST | VC')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
