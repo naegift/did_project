25b76f87c7648e89c79765a15a5faabbbed2b1e7
boram-blockchain <penguniee@gmail.com>
2024-03-08 15:53:41 +0900

    fix : slider autoplay speed, header fixed

--- frontend/src/components/molecules/MainSlider.tsx
+++ frontend/src/components/molecules/MainSlider.tsx
@@ -1,40 +1,40 @@
 import Slider from "react-slick";
+
 import "../../styles/slider.css";
 import "../../styles/slider2.css";
 import {
-  bannerImg3,
-  bannerImg4,
-  bannerImg5,
-  bannerImg6,
+    bannerImg3,
+    bannerImg4,
+    bannerImg5,
+    bannerImg6,
 } from "../../images/Banner";
 
 const MainSlider = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    speed: 5000,
-    autoplaySpeed: 500,
-    arrows: false,
-  };
-  return (
-    <div className="slick-slider pb-5">
-      <Slider {...settings}>
-        <div>
-          <img src={bannerImg4} alt="" />
+    const settings = {
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+    };
+    return (
+        <div className="slider-container slick-slider pb-5">
+            <Slider {...settings}>
+                <div>
+                    <img src={bannerImg4} alt="" />
+                </div>
+                <div>
+                    <img src={bannerImg5} alt="" />
+                </div>
+                <div>
+                    <img src={bannerImg6} alt="" />
+                </div>
+                <div>
+                    <img src={bannerImg3} alt="" />
+                </div>
+            </Slider>
         </div>
-        <div>
-          <img src={bannerImg5} alt="" />
-        </div>
-        <div>
-          <img src={bannerImg6} alt="" />
-        </div>
-        <div>
-          <img src={bannerImg3} alt="" />
-        </div>
-      </Slider>
-    </div>
-  );
+    );
 };
 
 export default MainSlider;
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,7 +1,4 @@
-import React, {
-  useEffect,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Pagination from "react-js-pagination";
 
@@ -12,195 +9,171 @@ import { cn } from "../utils/cn";
 import MainSlider from "../components/molecules/MainSlider";
 
 export interface Data {
-  nextPage: number;
-  products: [];
-  productsCount: number;
-  totalPages: number;
+    nextPage: number;
+    products: [];
+    productsCount: number;
+    totalPages: number;
 }
 
 export interface Product {
-  id: number;
-  title: string;
-  image: string;
-  price: number;
+    id: number;
+    title: string;
+    image: string;
+    price: number;
 }
 
 const Main: React.FC = () => {
-  const [product, setProduct] = useState<Product[]>([]);
-  const [latestProduct, setLatestProduct] = useState<Product[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [totalPage, setTotalPage] = useState<number>(1);
-  const [order, setOrder] = useState<string>("asc");
+    const [product, setProduct] = useState<Product[]>([]);
+    const [latestProduct, setLatestProduct] = useState<Product[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [totalPage, setTotalPage] = useState<number>(1);
+    const [order, setOrder] = useState<string>("asc");
 
-  const changePage = async (
-    pageNumber: number
-  ) => {
-    setPage(pageNumber);
-  };
+    const changePage = async (pageNumber: number) => {
+        setPage(pageNumber);
+    };
 
-  const mainData = async (
-    page: number
-  ) => {
-    try {
-      const response =
-        await axios.get<Data>(
-          `${
-            process.env.REACT_APP_API ||
-            process.env.REACT_APP_AWS
-          }/?page=${page}&order=${order}`
-        );
+    const mainData = async (page: number) => {
+        try {
+            const response = await axios.get<Data>(
+                `${
+                    process.env.REACT_APP_API || process.env.REACT_APP_AWS
+                }/?page=${page}&order=${order}`
+            );
 
-      console.log(response.data);
-      console.log(
-        response.data.products
-      );
-      setProduct(
-        response.data.products
-      );
-      setTotalPage(
-        response.data.totalPages
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            console.log(response.data);
+            console.log(response.data.products);
+            setProduct(response.data.products);
+            setTotalPage(response.data.totalPages);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const latestData = async () => {
-    try {
-      const latestRes = await axios.get<Data>(
-        `${
-          process.env.REACT_APP_AWS || process.env.REACT_APP_API
-        }/?page=1&order=desc`
-      );
-      console.log(latestRes.data.products);
-      setLatestProduct(latestRes.data.products);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const latestData = async () => {
+        try {
+            const latestRes = await axios.get<Data>(
+                `${
+                    process.env.REACT_APP_AWS || process.env.REACT_APP_API
+                }/?page=1&order=desc`
+            );
+            console.log(latestRes.data.products);
+            setLatestProduct(latestRes.data.products);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  useEffect((): void => {
-    mainData(page);
-    latestData();
-  }, [page, order]);
+    useEffect((): void => {
+        mainData(page);
+        latestData();
+    }, [page, order]);
 
-  const orderChange = (
-    selected: string
-  ) => {
-    setOrder(selected);
-  };
+    const orderChange = (selected: string) => {
+        setOrder(selected);
+    };
 
-  return (
-    <>
-      <MainSlider />
+    return (
+        <div className="mt-[97px]">
+            <MainSlider />
 
-      <div
-        className={cn(
-          "w-4/5 flex flex-row gap-5 py-8 px-20 mx-auto items-center justify-between ",
-          "note:w-full",
-          "tablet:w-full tablet:px-14",
-          "mobile:px-5 mobile:flex-col"
-        )}
-      >
-        <span className="text-xl font-extrabold">지금 올라온 NEW!</span>
-      </div>
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row gap-5 py-8 px-20 mx-auto items-center justify-between ",
+                    "note:w-full",
+                    "tablet:w-full tablet:px-14",
+                    "mobile:px-5 mobile:flex-col"
+                )}
+            >
+                <span className="text-xl font-extrabold">지금 올라온 NEW!</span>
+            </div>
 
-      <div
-        className={cn(
-          "w-4/5 flex flex-row pb-10 gap-7 px-20 mx-auto",
-          "note:w-full",
-          "tablet:w-full tablet:flex-wrap tablet:px-16 tablet:gap-12",
-          "mobile:flex-wrap mobile:px-6"
-        )}
-      >
-        <ProductList products={latestProduct} />
-      </div>
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row pb-10 gap-7 px-20 mx-auto",
+                    "note:w-full",
+                    "tablet:w-full tablet:flex-wrap tablet:px-16 tablet:gap-12",
+                    "mobile:flex-wrap mobile:px-6"
+                )}
+            >
+                <ProductList products={latestProduct} />
+            </div>
 
-      <div
-        className={cn(
-          "w-4/5 flex flex-row justify-evenly mx-auto gap-20 px-20 py-10",
-          "tablet:flex-col tablet:gap-5 tablet:px-10",
-          "mobile:flex-col mobile:gap-5  mobile:px-0 mobile:py-2"
-        )}
-      >
-        <img src={copy2} alt="" />
-        <img src={copy1} alt="" />
-      </div>
-      <div
-        className={cn(
-          "w-4/5 flex flex-row py-5 gap-5 px-20 mx-auto items-center justify-between",
-          "note:w-full",
-          "tablet:w-full tablet:px-14",
-          "mobile:px-5 mobile:flex-col"
-        )}
-      >
-        <span className="text-xl font-extrabold">전체 상품 리스트</span>
-        <div className="flex flex-row gap-8">
-          <Button
-            variant="basicBtn2"
-            size="md"
-            label="최신순"
-            onClick={() =>
-              orderChange("desc")
-            }
-          />
-          <Button
-            variant="basicBtn2"
-            size="md"
-            label="과거순"
-            onClick={() =>
-              orderChange("asc")
-            }
-          />
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row justify-evenly mx-auto gap-20 px-20 py-10",
+                    "tablet:flex-col tablet:gap-5 tablet:px-10",
+                    "mobile:flex-col mobile:gap-5  mobile:px-0 mobile:py-2"
+                )}
+            >
+                <img src={copy2} alt="" />
+                <img src={copy1} alt="" />
+            </div>
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row py-5 gap-5 px-20 mx-auto items-center justify-between",
+                    "note:w-full",
+                    "tablet:w-full tablet:px-14",
+                    "mobile:px-5 mobile:flex-col"
+                )}
+            >
+                <span className="text-xl font-extrabold">전체 상품 리스트</span>
+                <div className="flex flex-row gap-8">
+                    <Button
+                        variant="basicBtn2"
+                        size="md"
+                        label="최신순"
+                        onClick={() => orderChange("desc")}
+                    />
+                    <Button
+                        variant="basicBtn2"
+                        size="md"
+                        label="과거순"
+                        onClick={() => orderChange("asc")}
+                    />
+                </div>
+            </div>
+
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row py-4 gap-7 px-20 mx-auto ",
+                    "note:w-full",
+                    "tablet:w-full tablet:flex-wrap tablet:px-16 tablet:gap-12",
+                    "mobile:flex-wrap mobile:px-6"
+                )}
+            >
+                <ProductList products={product} />
+            </div>
+
+            <div
+                className={cn(
+                    "w-4/5 flex flex-row py-5 gap-5 mx-auto justify-center items-center",
+                    "mobile:w-full"
+                )}
+            >
+                <Pagination
+                    activePage={page}
+                    itemsCountPerPage={5}
+                    totalItemsCount={totalPage * 5}
+                    pageRangeDisplayed={5}
+                    prevPageText={"‹"}
+                    nextPageText={"›"}
+                    onChange={(pageNumber) => changePage(pageNumber)}
+                    innerClass="flex flex-row py-5 justify-center items-center gap-2 mobile:gap-[5px] moblie:w-full"
+                    itemClass="inline-block w-10 h-10 border border-gray-300 flex justify-center items-center rounded-3xl hover:bg-[#ff4400] hover:text-white hover:border-none"
+                    activeClass="pagination-active text-black"
+                />
+            </div>
+
+            <div className="w-pull">
+                <img
+                    src={bannerImg2}
+                    alt=""
+                    className="w-[100%] mobile:h-[150px]"
+                />
+            </div>
         </div>
-      </div>
-
-      <div
-        className={cn(
-          "w-4/5 flex flex-row py-4 gap-7 px-20 mx-auto ",
-          "note:w-full",
-          "tablet:w-full tablet:flex-wrap tablet:px-16 tablet:gap-12",
-          "mobile:flex-wrap mobile:px-6"
-        )}
-      >
-        <ProductList
-          products={product}
-        />
-      </div>
-
-      <div
-        className={cn(
-          "w-4/5 flex flex-row py-5 gap-5 mx-auto justify-center items-center",
-          "mobile:w-full"
-        )}
-      >
-        <Pagination
-          activePage={page}
-          itemsCountPerPage={5}
-          totalItemsCount={
-            totalPage * 5
-          }
-          pageRangeDisplayed={5}
-          prevPageText={"‹"}
-          nextPageText={"›"}
-          onChange={(pageNumber) =>
-            changePage(pageNumber)
-          }
-          innerClass="flex flex-row py-5 justify-center items-center gap-2 mobile:gap-[5px] moblie:w-full"
-          itemClass="inline-block w-10 h-10 border border-gray-300 flex justify-center items-center rounded-3xl hover:bg-[#ff4400] hover:text-white hover:border-none"
-          activeClass="pagination-active text-black"
-        />
-      </div>
-
-      <div className="w-pull">
-        <img
-          src={bannerImg2}
-          alt=""
-          className="w-[100%] mobile:h-[150px]"
-        />
-      </div>
-    </>
-  );
+    );
 };
 
 export default Main;
