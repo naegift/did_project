79b79e434d8f01c078428bc745602a30167d7f87
아기참돔 <wjdtmdry88@gmail.com>
2024-03-15 15:54:41 +0900

    fix : notification list

--- frontend/src/components/templates/MyNotificationBox.tsx
+++ frontend/src/components/templates/MyNotificationBox.tsx
@@ -16,13 +16,22 @@ const MyNotificationBox: React.FC<MyNotificationBoxProps> = ({
     return null;
   }
 
-  // 모달의 배경 클릭 시 모달 닫기
+  const formatTimestamp = (timestamp: number) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString("ko-KR", {
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  };
+
   const handleModalClose = (e: React.MouseEvent) => {
     e.stopPropagation();
     toggleModal();
   };
 
-  // 모달 컨텐츠 클릭 시 이벤트 전파 방지
   const handleModalContentClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
@@ -31,19 +40,37 @@ const MyNotificationBox: React.FC<MyNotificationBoxProps> = ({
     <div
       id="myNotificationModal"
       className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center z-50"
-      onClick={handleModalClose} // 모달 배경 클릭 이벤트 추가
+      onClick={handleModalClose}
     >
       <div
         className="relative p-5 border w-96 shadow-lg rounded-md bg-white"
-        onClick={handleModalContentClick} // 모달 컨텐츠 클릭 이벤트 추가
+        onClick={handleModalContentClick}
       >
         <div className="modal-content">
           <div id="myNotificationBox" className="mt-3 text-center">
-            {inboxNotifications.map((notification, index) => (
-              <div key={index} className="py-2 px-4 bg-gray-100 my-2 rounded">
-                {notification.message}
-              </div>
-            ))}
+            {inboxNotifications.map((notification, index) => {
+              const timestampMatch =
+                notification.message.match(/\[timestamp: (\d+)\]/);
+              const timestamp = timestampMatch
+                ? parseInt(timestampMatch[1], 10)
+                : null;
+              const formattedMessage = timestamp
+                ? notification.message.replace(
+                    /\[timestamp: \d+\]/,
+                    formatTimestamp(timestamp)
+                  )
+                : notification.message;
+
+              return (
+                <div
+                  key={index}
+                  className="py-2 px-4 bg-gray-100 my-2 rounded"
+                  style={{ whiteSpace: "pre-line" }}
+                >
+                  {formattedMessage}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
